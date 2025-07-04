5fc3e889b06a19d5742d20f8aadef7be079302d1
vrouker <vrouker@gmail.com>
2025-07-03 21:06:57 -0400

    updated db queries for score to add get, post, and update scores

--- db/queries/score.js
+++ db/queries/score.js
@@ -9,3 +9,32 @@ export async function createScore({user_id, score, created_at}){
     const {rows: scores} = await db.query(sql, [user_id, score, created_at]);
     return scores;
 };
+
+export async function getHighestScore({id}){
+    const sql = `
+        SELECT * FROM score WHERE user_id = $1;
+    `;
+    const {rows: [highScore],} = await db.query(sql, [id]);
+    return highScore;
+};
+
+export async function postNewScore(){
+    const sql = `
+        INSERT INTO score (user_id, score, created_at)
+        VALUES ($1, $2, $3)
+        RETURNING *:
+    `;
+    const {rows: newScore} = await db.query(sql, [user_id, score, created_at]);
+    return newScore[0];
+};
+
+export async function updateHighestScore({score, created_at, id}){
+    const sql = `
+        UPDATE score 
+        SET score = $1, created_at = $2
+        WHERE user_id = $3
+        RETURNING *;
+        `;
+    const result = await db.query(sql, [score, created_at, id]);
+    return result.rows[0];
+};
