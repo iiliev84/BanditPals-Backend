76995cbf34a5962413ba5e16c039a44c32ed90a3
vrouker <vrouker@gmail.com>
2025-07-10 19:48:00 -0400

    changed the wording on the achievements

--- db/seed.js
+++ db/seed.js
@@ -14,7 +14,7 @@ async function seed() {
     await createScore({user_id: user1.id, score: 25, created_at: "2025-07-03T20:02:00.000Z"});
     await createScore({user_id: user2.id, score: 30, created_at: "2025-07-04T20:02:00.000Z"});
     await createAchievement({name: "First Trash", description: "Collect your first piece of trash"});
-    await createAchievement({name: "Clean Freak", description:"Collect 5 pieces of trash in one game!"});
+    await createAchievement({name: "Clean Freak", description:"Collect 5 pieces of trash in one round of BanditPals!"});
     await createAchievement({name: "Eco Hero", description: "Collect all pieces of trash in one round of BanditPals!"});
     await postUserAchievement({user_id: 1, achievement_id: 1, unlocked_at: "2025-10-07T20:02:00.000Z"});
 }
